fcb870293216f1795e87238d8ba200ec1f92cf41
DavidMoralesLuis <dmoralesluis@danielcastelao.org>
2023-01-26 13:02:32 +0100

    2

--- src/main/java/tutorial/Main.java
+++ src/main/java/tutorial/Main.java
@@ -14,10 +14,11 @@ public class Main {
         em.getTransaction().begin();
 
         // Store 1000 Point objects in the database:
-//        for (int i = 0; i <= 10; i++) {
-//            Point p = new Point(i,i, i);
-//            em.persist(p);
-//        }
+        for (int i = 0; i <= 10; i++) {
+            Point p = new Point(i,i, i);
+            em.persist(p);
+        }
+
 
 
 
@@ -30,32 +31,70 @@ public class Main {
         System.out.println("Average X: " + q2.getSingleResult());
 
         // Retrieve all the Point objects from the database:
-        TypedQuery<Point> query =
-                em.createQuery("SELECT p FROM Point p", Point.class);
+        TypedQuery<Point> query = em.createQuery("SELECT p FROM Point p", Point.class);
         List<Point> results = query.getResultList();
         for (Point p : results) {
             System.out.println(p);
         }
+        System.out.println();
 
         //amosar punto 10
         Query punto10 = em.createQuery("select p from Point p where p.id =10");
-        System.out.println("Punto 10: "+punto10.getSingleResult());
+        System.out.println("Punto 10(antes): "+punto10.getSingleResult());
 
-        try {
-            Query cambiarPunto= em.createQuery("update Point set y = y+2 where id = 10");
-            int cambioInt = cambiarPunto.executeUpdate();
-        }catch (TransactionRequiredException e){
-            System.out.println(e);
-        }
-
-
-        //amosar novo punto 10
-        Query newPunto10 = em.createQuery("select p from Point p where p.id =10");
-        System.out.println("Punto 10: "+newPunto10.getSingleResult());
-
-        // Close the database connection:
         em.getTransaction().commit();
         em.close();
         emf.close();
+
+
+        // Open a database connection
+        // (create a new database if it doesn't exist yet):
+        EntityManagerFactory emf2 =
+                Persistence.createEntityManagerFactory(
+                        "$objectdb/db/p2.odb");
+        EntityManager em2 = emf2.createEntityManager();
+        em2.getTransaction().begin();
+
+        //cambiar coor. del punto 10
+        Query cambiarPunto= em2.createQuery("update Point set y = y+2 where id = 10");
+        int cambioInt = cambiarPunto.executeUpdate();
+        System.out.println("Cambiamos la coor.Y del Punto 10");
+
+        //amosar novo punto 10
+        Query newPunto10 = em2.createQuery("select p from Point p where p.id =10");
+        System.out.println("Punto 10(despues): "+newPunto10.getSingleResult()+"\n");
+
+        // Close the database connection:
+        em2.getTransaction().commit();
+        em2.close();
+        emf2.close();
+
+
+        // Open a database connection
+        // (create a new database if it doesn't exist yet):
+//        EntityManagerFactory emf3 =
+//                Persistence.createEntityManagerFactory(
+//                        "$objectdb/db/p2.odb");
+//        EntityManager em3 = emf3.createEntityManager();
+//        em3.getTransaction().begin();
+//
+//        //borramos el Punto 5
+//        Query borrarPunto= em3.createQuery("delete from Point p where p.id = 5");
+//        int borrarInt = borrarPunto.executeUpdate();
+//        System.out.println("borramos el Punto 5");
+//
+//        // Retrieve all the Point objects from the database:
+//        TypedQuery<Point> query2 = em3.createQuery("SELECT p FROM Point p", Point.class);
+//        List<Point> results2 = query2.getResultList();
+//        for (Point p : results2) {
+//            System.out.println(p);
+//        }
+//
+//        // Close the database connection:
+//        em3.getTransaction().commit();
+//        em3.close();
+//        emf3.close();
+
+
     }
 }
